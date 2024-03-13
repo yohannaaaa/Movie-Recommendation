ec969b3124f9127663c7201ca5b33ea13561c03a
MichaelWorkineh_git <122392569+MichaelWorkineh@users.noreply.github.com>
2024-03-13 13:54:22 +0100

    Roughting

--- version1/src/App.jsx
+++ version1/src/App.jsx
@@ -1,34 +1,31 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { createBrowserRouter, Route, createRoutesFromElements } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
+import Signin from './pages/signIn';
+import Signout from './pages/signOut';
+import Details from './Page/Details';
 
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element = {<Home/>} />
+        <Route path="/Fav" element = {<Favorites/>} />
+        <Route path="/Signin" element = {<Signin/>} />
+        <Route path="/Signout" element = {<Signout/>} />
+        
+      </Route>
+    )
+  )
   return (
     <>
-    //sdfghjk
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <RouterProvider router={router}/>
     </>
   )
 }
---
+++ version1/src/pages/Signout.jsx
@@ -0,0 +1,7 @@
+export default function Signout(){
+    return(
+        <>
+            <h1>Signout</h1>
+        </>
+    )
+}
