0552c9c6e3ad0f5fd4c5f2247490f657cd0d7303
yohannaaaa <yohannamerawi@gmail.com>
2024-03-13 15:09:16 +0300

    search

--- version1/src/App.jsx
+++ version1/src/App.jsx
@@ -1,36 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavourite from './components/AddFavourites';
 
 function App() {
-  const [count, setCount] = useState(0)
+const [movies, setMovies] = useState([]);
+const [searchValue, setSearchValue] = useState('');
+
+const getMovieRequest = async (searchValue) => {
+  const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=13c8922f`;
+
+  const response = await fetch(url);
+  const responseJson = await response.json();
+
+  if(responseJson.Search){
+  setMovies(responseJson.Search);
+}
+};
+
+useEffect(() => {
+  getMovieRequest(searchValue);
+}, [searchValue]);
 
   return (
-    <>
-    //sdfghjk
-    //fghjk
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container-fluid movie-app'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+          <MovieListHeading heading = 'Movies'/>
+          <SearchBox searchValue = {searchValue} setSearchValue={setSearchValue}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className='row'>
+     <MovieList movies ={movies} favouriteComponent = {AddFavourite}/>
+     </div>
+    </div>
   )
 }
 
