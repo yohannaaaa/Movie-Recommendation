426ee61291ee23d7fc40b6ac7b07c4dc91aaf2e4
abraham hailebebriel <abraham>
2024-03-16 22:52:06 +0300

    login and signup page

--- version1/src/App.jsx
+++ version1/src/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { createBrowserRouter, Route, createRoutesFromElements } from "react-router-dom";
-import { RouterProvider } from "react-router-dom";
-import './App.css'
 
-import Home from './pages/Home';
-import Favorites from './pages/Favorites';
-import SignOut from './pages/Signout';
-import Signin from './pages/Signin';
-import Details from './pages/Details';
+import './App.css';
+import Loginpage from './pages/Loginpage.jsx';
+
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route>
-        <Route path="/" element = {<Home/>} />
-        <Route path="/Fav" element = {<Favorites/>} />
-        <Route path="/Signin" element = {<Signin/>} />
-        <Route path="/Signout" element = {<SignOut/>} />
-        <Route path="/Details" element = {<Details/>} />
-      </Route>
-    )
-  )
   return (
-    <>
-        <RouterProvider router={router}/>
-    </>
-  )
+    <div >
+     
+     <Loginpage/>
+    </div>
+   
+  );
 }
 
-export default App
+export default App;
