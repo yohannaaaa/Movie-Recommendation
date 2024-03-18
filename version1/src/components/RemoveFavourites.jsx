4126deefc583dce342b0d823025b5f98951faa65
yohannaaaa <yohannamerawi@gmail.com>
2024-03-18 20:43:31 +0300

    last

--- version1/src/components/RemoveFavourites.jsx
+++ version1/src/components/RemoveFavourites.jsx
@@ -1,28 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const RemoveFavourites = () => {
-	return (
-		<>
-			<span className='mr-2'>Remove from favourites</span>
-			<svg
-				width='1em'
-				height='1em'
-				viewBox='0 0 16 16'
-				class='bi bi-x-square'
-				fill='currentColor'
-				xmlns='http://www.w3.org/2000/svg'
-			>
-				<path
-					fill-rule='evenodd'
-					d='M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z'
-				/>
-				<path
-					fill-rule='evenodd'
-					d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z'
-				/>
-			</svg>
-		</>
-	);
-};
+    return (
+        <>
+        <svg 
+                 xmlns="http://www.w3.org/2000/svg" 
+                 width="16" 
+                 height="16" 
+                 fill="currentColor" 
+                 class="bi bi-trash3-fill" 
+                 viewBox="0 0 16 16"
+        >
+            <path
+             d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+</svg>
+<span className="mr-2">Remove</span>
+        </>
+        
 
+    )
+}
 export default RemoveFavourites;
